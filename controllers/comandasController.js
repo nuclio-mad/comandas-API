80ab4bf0f35d44acb75528dcc43edebcba125155
Daniel Herrero Hernando <danielherrerohernando@gmail.com>
2022-05-25 17:03:23 +0200

    chore: getting repo ready for class

--- controllers/comandasController.js
+++ controllers/comandasController.js
@@ -2,41 +2,17 @@ const mongoose = require('mongoose');
 const Comanda = require('../models/ComandasModel');
 
 const comandasController = {
-    getComandas: async function(req, res) {
-        // const listaComandas = await Comanda.find().populate('camarero').populate('cliente');
-        Comanda.find().populate('camarero').populate('cliente')
-            .then(listaComandas => {
-                res.json(listaComandas);
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    },
-    addComanda: function(req, res) {
-        const { camareroId, clientId, name } = req.body
+    getComandas: async function (req, res) {
 
-        const newComanda = new Comanda();
-        newComanda.camarero = camareroId;
-        newComanda.cliente = clientId;
-        newComanda.name = name;
+    },
+    addComanda: function (req, res) {
 
-        newComanda.save( (err, savedInfo) => {
-            if(err) {
-                console.log('Ha ocurrido un error', err);
-            }
-            console.log(savedInfo)
-            res.json(savedInfo);
-        } );
     },
-    deleteComanda: async function(req, res) {
-        const { id } = req.body
-        await Comanda.deleteOne({_id: id })
-        res.send('holaaaaa')
+    deleteComanda: async function (req, res) {
+
     },
-    updateComanda: async function(req, res) {
-        const { id, name } = req.body
-        const data = await Comanda.findOneAndUpdate({_id: id}, { name }, {new: true})
-        res.json(data)
+    updateComanda: async function (req, res) {
+
     }
 };
 
--- models/CamareroModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const CamareroSchema = new Schema({
-    name: String,
-});
-
-module.exports = mongoose.model('Camarero', CamareroSchema);
